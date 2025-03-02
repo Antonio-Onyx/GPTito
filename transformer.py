a53f2cb43bd8554aa5d4e479a115b96915818c66
Gerardo <ant0ni0lnx31@gmail.com>
2025-03-01 19:46:11 -0600

    adding the transformer to repo

---
+++ transformer.py
@@ -0,0 +1,166 @@
+import torch.nn as nn
+import torch
+import math
+import torch.nn.functional as F
+
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, vocab_size: int, d_model: int) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_length):
+        super().__init__()
+
+        pe = torch.zeros(max_seq_length, d_model)
+        position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float) * -(math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        self.register_buffer('pe', pe.unsqueeze(0))
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super().__init__()
+
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+
+        self.num_heads = num_heads
+        self.d_model = d_model
+        self.head_dim = d_model // num_heads
+        self.query_linear = nn.Linear(d_model, d_model, bias=False)
+        self.key_linear = nn.Linear(d_model, d_model, bias=False)
+        self.value_linear = nn.Linear(d_model, d_model, bias=False)
+        self.output_linear = nn.Linear(d_model, d_model)
+
+    def split_heads(self, x, batch_size):
+        seq_length = x.size(1)
+        x = x.view(batch_size, seq_length, self.num_heads, self.head_dim)
+        return x.permute(0,2,1,3)
+    
+    def compute_attenttion(self, query, key, value, mask=None):
+        scores = torch.matmul(query, key.transpose(-2,-1)) / (self.head_dim ** 0.5)
+        #print("shape de scores:", scores.shape)
+        if mask is not None:
+        #    print("Shape de mask:", mask.shape)
+            scores = scores.masked_fill(mask == 0, float('-inf'))
+        attention_weights = F.softmax(scores, dim=-1)
+        return torch.matmul(attention_weights, value)
+    
+    def combine_heads(self, x, batch_size):
+        x = x.permute(0,2,1,3).contiguous()
+        return x.view(batch_size, -1, self.d_model)
+    
+    def forward(self, query, key, value, mask=None):
+        batch_size = query.size(0)
+        query = self.split_heads(self.query_linear(query), batch_size)
+        key = self.split_heads(self.key_linear(key), batch_size)
+        value = self.split_heads(self.value_linear(value), batch_size)
+
+        attention_weigths = self.compute_attenttion(query, key, value, mask)
+
+        output = self.combine_heads(attention_weigths, batch_size)
+        return self.output_linear(output)
+    
+# Feed-forward sublayer in encoder layers
+class FeedForwardSublayer(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super().__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+    
+# Encoder layer
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super().__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.ff_sublayer = FeedForwardSublayer(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, src_mask):
+        attn_output = self.self_attn(x, x, x, src_mask)
+        x = self.norm1(x + self.dropout(attn_output))
+        ff_output = self.ff_sublayer(x)
+        x = self.norm2(x + self.dropout(ff_output))
+        return x
+    
+# Encoder transformer body
+class TranformerEncoder(nn.Module):
+    def __init__(self, vocab_size, d_model, num_layers, num_heads, d_ff, dropout, max_seq_length):
+        super().__init__()
+        self.embedding = InputEmbeddings(vocab_size, d_model)
+        self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
+        self.layers = nn.ModuleList(
+            [EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)]
+        )
+
+    def forward(self, x, src_mask):
+        x = self.embedding(x)
+        x = self.positional_encoding(x)
+        for layer in self.layers:
+            x = layer(x, src_mask)
+        return x
+    
+# Encoder transformers head
+class ClassifierHead(nn.Module):
+    def __init__(self, d_model, num_classes):
+        super().__init__()
+        self.fc = nn.Linear(d_model, num_classes)
+
+    def forward(self, x):
+        logits = self.fc(x)
+        return F.log_softmax(logits, dim=-1)
+    
+class RegressionHead(nn.Module):
+    def __init__(self, d_model, output_dim=1, activation=None):
+        super().__init__()
+        self.fc = nn.Linear(d_model, output_dim)
+        self.activation = activation
+
+    def forward(self, x):
+        output = self.fc(x)
+        if self.activation == "sigmoid":
+            return torch.sigmoid(output)
+        elif self.activation == "relu":
+            return F.relu(output)
+        return output
+    
+# Ahora viene el Decoder Layer
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout):
+        super().__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.cross_attn = MultiHeadAttention(d_model, num_heads)
+        self.ff_sublayer = FeedForwardSublayer(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, encoder_output, tgt_mask):
+        attn_output = self.self_attn(x,x,x, tgt_mask)
+        x = self.norm1(x + self.dropout(attn_output))
+
+        cross_attn_output = self.cross_attn(x, encoder_output, encoder_output)
+        x = self.norm2(x + self.dropout(cross_attn_output))
+
+        ff_output = self.ff_sublayer(x)
+        x = self.norm3(x + self.dropout(ff_output))
+        return x

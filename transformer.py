a5cb74a478f50a08899a27bc4e06d7e8b8fa9f77
Gerardo <ant0ni0lnx31@gmail.com>
2025-03-11 13:04:19 -0600

    aplicando ultimos cambios y funciones añadidas

--- transformer.py
+++ transformer.py
@@ -110,13 +110,14 @@ class TranformerEncoder(nn.Module):
         self.layers = nn.ModuleList(
             [EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)]
         )
+        self.norm = nn.LayerNorm(d_model)
 
     def forward(self, x, src_mask):
         x = self.embedding(x)
         x = self.positional_encoding(x)
         for layer in self.layers:
             x = layer(x, src_mask)
-        return x
+        return self.norm(x)
     
 # Encoder transformers head
 class ClassifierHead(nn.Module):
@@ -163,4 +164,21 @@ class DecoderLayer(nn.Module):
 
         ff_output = self.ff_sublayer(x)
         x = self.norm3(x + self.dropout(ff_output))
+        return x
+    
+# cuerpo del TransformerDecoder
+class TransformerDecoder(nn.Module):
+    def __init__(self, vocab_size, d_model, num_layers, num_heads, d_ff, dropout, max_seq_length) -> None:
+        super().__init__()
+        self.embedding = InputEmbeddings(vocab_size, d_model)
+        self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
+        self.layers = nn.ModuleList(
+            [DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)]
+        )
+
+    def forward(self, x, encoder_output, tgt_mask):
+        x = self.embedding(x)
+        x = self.positional_encoding(x)
+        for layer in self.layers:
+            x = layer(x, encoder_output, tgt_mask)
         return x
